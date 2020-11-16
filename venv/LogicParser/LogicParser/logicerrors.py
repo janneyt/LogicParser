3a151640de748f70c9bfba17e10a048b64ef43bb
Ted Janney <janneyt@oregonstate.edu>
2020-11-16 13:44:43 -0800

    Added exception handling and one more error code. Wrote tests for all current uses of class LogicErrors

--- venv/LogicParser/LogicParser/logicerrors.py
+++ venv/LogicParser/LogicParser/logicerrors.py
@@ -8,28 +8,38 @@ class LogicErrors():
     so the program can just simply move on to asking for more input.
 
     Data members - each one should be named for the action that caused the error:
-        Public:
-
-
         Private:
             A dictionary or map with error codes and the verbose error it should return
 
     Methods:
         Public:
             get_error(some_string_error_description)
-        Private:
-            check_for_errors(some_string_error_description) -> dictionary or map to look up error codes and match
-                errors.
-            undefined_error(some_string_error_description) -> if check_for_errors() doesn't have a key with the passed
-                error code, it throws an undefined error.
+
 
 
     '''
     def __init__(self, error_code):
         self._error_code = error_code
-        self.wrong_input = "\n\nYou have attempted unrecognized input. Type -h for help or -u for usage.\n\n"
 
+        '''
+        Below is the error dictionary that makes this whole class worthwhile.
+        
+        PLEASE UPDATE.
+        '''
+        self.error_dict = {
+            "wrong input": "\n\nYou have attempted unrecognized input. Type -h for help or -u for usage.\n\n",
+            "not recognized": "\n\nHey! Whatever you just did is an unrecognized error. Pathetic.\n\n"
+        }
     def get_error(self, error_code):
-        return self.wrong_input
+        try:
+            return self.error_dict[error_code.lower()]
 
-print(LogicErrors("Wrong Input").wrong_input)
+        # The exception handling is designed to just pass an error code back. In this way, no matter what the
+        # function returns a string. However, if other code is for some reason relying on this error handling to
+        # fail, it won't. Thus, always expect a message back.
+        except Exception as e:
+            if type(e) is KeyError:
+                return self.error_dict["not recognized"]
+            elif type(e) is AttributeError:
+                return self.error_dict["not recognized"]
+            return e
--- venv/LogicParser/LogicParser/test_logicerrors.py
+++ venv/LogicParser/LogicParser/test_logicerrors.py
@@ -10,16 +10,36 @@ class WarningClassExists(unittest.TestCase):
 
     def test_LogicErrorsRequiredErrorCode(self):
         logic_error = LogicErrors("Wrong Input")
-        self.assertTrue(logic_error.get_error("Wrong Input"))
+        self.assertTrue(logic_error.get_error("wrong input"))
 
     def test_LogicErrorsWrongInputErrorExists(self):
         logic_error = LogicErrors("Wrong Input")
         # So I'm just testing the existing of the wrong_input error message
-        self.assertTrue(logic_error.wrong_input)
+        self.assertTrue(logic_error.error_dict["wrong input"])
 
     def test_LogicErrorsWrongInputReturnsWarning(self):
         logic_error = LogicErrors("Wrong Input")
-        self.assertEqual(logic_error.get_error("Wrong Input"), logic_error.wrong_input)
+        self.assertEqual(logic_error.get_error("wrong input"), logic_error.error_dict["wrong input"])
+
+    def test_LogicErrorsWrongInputCapitalization(self):
+        logic_error = LogicErrors("Wrong Input")
+        self.assertEqual(logic_error.get_error("wrong Input"), logic_error.error_dict["wrong input"])
+
+    def test_LogicErrorsWrongInputInteger(self):
+        logic_error = LogicErrors("wrong input")
+        self.assertRaises(TypeError, logic_error.get_error(1))
+
+    def test_LogicErrorsWrongInputNonexistentCode(self):
+        logic_error = LogicErrors("wrong input")
+        self.assertEqual(logic_error.get_error("all the elephants must die"), logic_error.get_error("not recognized"))
+
+    def test_LogicErrorsWrongInputNone(self):
+        logic_error = LogicErrors("wrong input")
+        self.assertEqual(logic_error.get_error(None), logic_error.get_error("not recognized"))
+
+    def test_LogicErrorsErrorDictionaryExists(self):
+        logic_error = LogicErrors("Wrong Input")
+        self.assertTrue(logic_error.error_dict)
 
 
 if __name__ == '__main__':
