25342f124531ebe7702e6e159d41d5177d09258d
Ted Janney <janneyt@oregonstate.edu>
2020-11-15 18:32:54 -0800

    Setup a few more tests. Put in a few function stubs.

--- venv/LogicParser/LogicParser/logicparser.py
+++ venv/LogicParser/LogicParser/logicparser.py
@@ -16,15 +16,42 @@ class LogicParser(object):
 
         '''
         self._message = message
+        self._wrong_letter = "The only valid truth table values are T and F. Please make sure to adhere to these guidelines."
         self._warning = "Something has gone wrong. Please be advised that you have passed some form of wrong input."
 
 
-    def begin_parsing(self):
+    def begin_parsing(self, message):
         '''
         Takes an object, the current truth query, and assigns it to the correct module of the program.
 
-        Object -> Function (or object.method)
-        :return: a function call
-        '''
-        pass
+        >>> LogicParser.begin_parsing(LogicParser('T -tt'),'T -tt')
+        'T'
 
+        >>> parse_list = ['T -tt', 'F -tt']
+        >>> [LogicParser.begin_parsing(LogicParser('T -tt'), x) for x in parse_list]
+        ['T','F']
+
+        >>> LogicParser.begin_parsing(LogicParser('T'), 'T')
+        'You are missing a command code. If you do not know what code to use, please type -h into the
+        console.'
+
+        >>> LogicParser.begin_parsing(LogicParser('T'),True)
+        Traceback (most recent call last):
+        ...
+        TypeError:
+
+        Object, String -> String
+        :return: a string
+        '''
+        string_message = isinstance(message, str)
+        if string_message:
+            return 'T'
+        else:
+            get_input()
+
+def get_input():
+    pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- venv/LogicParser/tests/tests.py
+++ venv/LogicParser/tests/tests.py
@@ -1,5 +1,6 @@
 import unittest
-from LogicParser.LogicParser.logicparser import LogicParser
+from LogicParser.LogicParser.logicparser import LogicParser, get_input
+
 
 class LogicParserExists(unittest.TestCase):
 
@@ -7,10 +8,16 @@ class LogicParserExists(unittest.TestCase):
         self.func = LogicParser('T')
 
     def test_begin_parsing_exists(self):
-        self.assertEquals(LogicParser.begin_parsing('T -tt'),'T')
+        self.assertEquals(LogicParser.begin_parsing(self, 'T -tt'),'T')
 
     def test_begin_parsing_single_false(self):
-        self.assertEquals(LogicParser.begin_parsing('F -tt'),'F')
+        self.assertEquals(LogicParser.begin_parsing(self, 'F -tt'),'F')
+
+    def test_begin_parsing_wrong_input(self):
+        self.assertEquals(LogicParser.begin_parsing(self, True), get_input())
+
+    def test_begin_parsing_wrong_characters(self):
+        self.assertEquals(LogicParser.begin_parsing(self, 'TL -tt'),"The only valid truth table values are T and F. Please make sure to adhere to these guidelines.")
 
 
 if __name__ == '__main__':
